fd65b18471ac000231eee69044fcc8a8fe2ca4ba
Petar <iosdev.petar@gmail.com>
2025-02-11 00:40:32 +0100

    Initial comit

---
+++ AvoSpot/AvoSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvoSpotApp.swift
+//  AvoSpot
+//
+//  Created by Petar  on 11.2.25..
+//
+
+import SwiftUI
+
+@main
+struct AvoSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
